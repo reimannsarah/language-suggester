cade0fe3ba080492a96df89b8fad68a12a7dab19
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-24 10:08:05 -0700

    add event listener, define languageresult variables

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 function userInput (event) {
   event.preventDefault ();
   const name = document.querySelector("#inputname").value;
-  const style = document.querySelector("input[name='stle']:checked").value;
+  const style = document.querySelector("input[name='style']:checked").value;
   const movie = document.querySelector("input[name='movie']:checked").value;
   const scenario = document.querySelector("input[name='scenario']:checked").value;
   const song = document.querySelector("input[name='song']:checked").value;
   const hygiene = document.querySelector("input[name='hygiene']:checked").value;
+  languageresult (style, movie, scenario, song, hygiene);
+  console.log("name", name);
 }
+
+function languageresult (style, movie, scenario, song, hygiene) {
+  const thanks = document.getElementById ("thanks");
+
+  thanks.innerText = `${name}`;
+
+}
+
 
 window.addEventListener("load", function() {
   document.querySelector("form").addEventListener("submit", userInput)
