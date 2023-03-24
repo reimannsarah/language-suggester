f822e6a7782c0ab19b43512190450f11f9d66c4c
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-24 15:14:47 -0700

    add background color, font family

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// function userInput (event) {
-//   event.preventDefault ();
-// }
-
 function languageResult (event) {
-  event.preventDefault ();
+  event.preventDefault();
   const result = document.querySelector("#result");
-  // result.setAttribute("class", "hidden");
   const thanks = document.getElementById("thanks"); 
   const name = document.querySelector("#inputname").value;
   
@@ -15,35 +10,42 @@ function languageResult (event) {
   const song = document.querySelector("input[name='song']:checked").value;
   const hygiene = document.querySelector("input[name='hygiene']:checked").value;
 
-  
   thanks.innerText = name;
 
   if (style==="style3" && movie === "movie2" && scenario === "scenario3" && song === "song1" && hygiene === "hygiene2") {
     result.removeAttribute("class");
     language.innerText="Python";  
   } else if (style === "style2" && movie === "movie3" && scenario === "scenario1" && song === "song3" && hygiene === "hygiene2") {
+    result.removeAttribute("class");
     language.innerText = "C#";
   } else if (style === "style1" && movie === "movie2" && scenario === "scenario3" && song === "song1" && hygiene === "hygiene2") {
+    result.removeAttribute("class");
     language.innerText = "Ruby";
   } else if (style === "style3" && movie === "movie2" && scenario === "scenario1" && song === "song3" && hygiene === "hygiene2") {
+    result.removeAttribute("class");
     language.innerText = "Python";
   } else if (style === "style3" && movie === "movie3" && scenario === "scenario3" && song === "song2" && hygiene === "hygiene1") {
+    result.removeAttribute("class");
     language.innerText = "C#";
   } else if (style === "style2" && movie === "movie3" && scenario === "scenario1" && song === "song2" && hygiene === "hygiene3") {
+    result.removeAttribute("class");
     language.innerText = "Ruby";
   } else if (style === "style1" && movie === "movie1" && scenario === "scenario1" && song === "song1" && hygiene === "hygiene1") {
     language.innerText = "Rust";
+    result.removeAttribute("class");
   } else if (style === "style2" && movie === "movie2" && scenario === "scenario2" && song === "song2" && hygiene === "hygiene2") {
     language.innerText = "JavaScript";
+    result.removeAttribute("class");
   } else if (style === "style3" && movie === "movie3" && scenario === "scenario3" && song === "song3" && hygiene === "hygiene3") {
+    result.removeAttribute("class");
     language.innerText = "Go";
   } else if (style === "style2" && movie === "movie2" && scenario === "scenario2" && song === "song2" && hygiene === "hygiene3") {
+    result.removeAttribute("class");
     language.innerText = "Rust";
   } else {
+    result.removeAttribute("class");
     language.innerText = "Swift";
-  }
-  
-  
+  }  
 }
 
 window.addEventListener("load", function() {
